eaff5ef6dd9ed0781062f443df23790732f0e79f
michal <michalwols@gmail.com>
2018-01-02 13:11:35 -0500

    fall through to python lanms if can't build lanms

--- eval.py
+++ eval.py
@@ -5,8 +5,15 @@ import os
 import numpy as np
 import tensorflow as tf
 
-import locality_aware_nms as nms_locality
-import lanms
+try:
+    import lanms
+    non_max_suppress = lanms.merge_quadrangle_n9
+except Exception as e:
+    import warnings
+    warnings.warn("Couldn't import lanms, might not have been built. Using "
+                  "python version instead")
+    import locality_aware_nms
+    non_max_suppress = locality_aware_nms.nms_locality
 
 tf.app.flags.DEFINE_string('test_data_path', '/tmp/ch4_test_images/images/', '')
 tf.app.flags.DEFINE_string('gpu_list', '0', '')
@@ -95,7 +102,7 @@ def detect(score_map, geo_map, timer, score_map_thresh=0.8, box_thresh=0.1, nms_
     # nms part
     start = time.time()
     # boxes = nms_locality.nms_locality(boxes.astype(np.float64), nms_thres)
-    boxes = lanms.merge_quadrangle_n9(boxes.astype('float32'), nms_thres)
+    boxes = non_max_suppress(boxes.astype('float32'), nms_thres)
     timer['nms'] = time.time() - start
 
     if boxes.shape[0] == 0:
